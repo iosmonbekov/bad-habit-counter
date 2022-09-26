1521f6eb989ccefead5bcf5fc8f0eb1facd429f9
iosmonbekov <iosmonbekov666@gmail.com>
2022-09-26 23:09:06 +0600

    removed basename and homepage

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import './index.css';
 import HomePage from './views/HomePage';
 import NotFountPage from './views/NotFoundPage';
 import WelcomePage from './views/WelcomePage';
+import SignInPage from './views/SignInPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename='/bad-habit-counter'>
+  <BrowserRouter>
     <div className='container mx-auto px-4 h-full'>
       <Routes>
         <Route path='/' element={<WelcomePage />} />
         <Route path='/home' element={<HomePage />} />
+        <Route path='/login' element={<SignInPage />} />
 
         <Route path="*" element={<NotFountPage />} />
       </Routes>
